8f920424d7b4c98582b0c7d111e0688471313ba2
liuqingwan0612 <haiyanliu211@gmail.com>
2024-06-03 16:39:38 +0900

    add admin-html

---
+++ admin-html/admin_register_scripts.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const togglePasswordButton = document.querySelector(".toggle-password");
+    const passwordField = togglePasswordButton.previousElementSibling;
+    const icon = togglePasswordButton.querySelector('i');
+
+    togglePasswordButton.addEventListener("click", function() {
+        if (passwordField.type === "password") {
+            passwordField.type = "text";
+            icon.classList.remove('fa-eye');
+            icon.classList.add('fa-eye-slash');
+        } else {
+            passwordField.type = "password";
+            icon.classList.remove('fa-eye-slash');
+            icon.classList.add('fa-eye');
+        }
+    });
+});
+
+function previewIcon(event) {
+    const reader = new FileReader();
+    reader.onload = function() {
+        const iconPlaceholder = document.getElementById('icon-placeholder');
+        const iconImg = document.createElement('img');
+        iconImg.src = reader.result;
+        iconPlaceholder.innerHTML = '';
+        iconPlaceholder.appendChild(iconImg);
+    };
+    reader.readAsDataURL(event.target.files[0]);
+}
